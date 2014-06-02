616605e079994d66ed66ae98bb297c3623a190d8
Mike Santiago <miketheripper1@gmail.com>
2014-06-01 20:22:34 -0400

    Started work on the actual manager

--- EpisodeManager/MainWindow.xaml.cs
+++ EpisodeManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -17,11 +19,47 @@ namespace EpisodeManager
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void populateListView()
+        {
+            ListViewItem lvi = new ListViewItem();
+            DirectoryInfo dinfo = new DirectoryInfo(@"E:\SMBX\worlds");
+            foreach(var d in dinfo.GetDirectories())
+            {
+                lvi = new ListViewItem();
+                lvi.Content = d.ToString();
+                localEpisodesList.Items.Add(lvi);
+            }
+        }
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (string textt in localEpisodesList.SelectedItems)
+            {
+                MessageBox.Show(textt);
+            }
+            
+
+            if (File.Exists(@"E:\SMBX\worlds\" + localEpisodesList.SelectedItem + @"\project.index") == true)
+            {
+                //
+            }
+            else
+            {
+                episodeName.Text = localEpisodesList.SelectedValue.ToString();
+                authorLabel.Text = "";
+                descriptionLabel.Text = "This episode doesn't seem to have an index file associated with it.";
+            }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            populateListView();
         }
     }
 }
